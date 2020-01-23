c778132f12e580413b885861178825b745b5d686
birdx0810 <birdx0810@gmail.com>
2020-01-23 16:32:14 +0800

    update db

--- database.py
+++ database.py
@@ -15,14 +15,21 @@ import click
 # Import system modules
 import datetime, re
 
+config = {
+    'host': '192.168.0.220',
+    'user': 'mb-admin',
+    'password': '04bu2xeK',
+    'database': 'medbot_db',
+}
+
 # Connect to DB
-def connect(path):
+def connect():
     '''
-    Initiallize the connection to DB
+    Initialize the connection to DB
     Prints an error if fail to connect
     '''
     try:
-        conn = mariadb.connect(user='user', password='password', database='medbot')
+        conn = mariadb.connect(**config)
     except Error as e:
         print(e)
     return conn
@@ -32,18 +39,35 @@ def query(conn, qry, var):
     '''
     Function for executing `SELECT * FROM table WHERE var0=foo, var1=bar`
     '''
-    c = conn.cursor()
-    c.execute(qry, var)
-    rows = c.fetchall()
+    try:
+        conn = mariadb.connect(**config)
+        c = conn.cursor()
+        c.execute(qry, var)
+        rows = c.fetchall()
+    except Error as e:
+        print(e)
+    finally:
+        c.close()
+        conn.close()
     return rows
 
 def update(conn, qry, var):
     '''
     Function for updating DB
     '''
-    c = conn.cursor()
-    c.execute(qry, var)
-    conn.commit()
+    try:
+        conn = mariadb.connect(**config)
+        c = conn.cursor()
+        c.execute(qry, var)
+    except Error as e:
+        conn.rollback()
+        print(e)
+    else:
+        conn.commit()
+        print("Update successful")
+    finally:
+        c.close()
+        conn.close()
 
 def delete(conn, qry, var):
     '''
@@ -52,11 +76,12 @@ def delete(conn, qry, var):
     c = conn.cursor()
     c.execute(qry, var)
 
-# Log messages to DB
+# Other functions
 def log(userid, message, sess):
     '''
     Log user messages and the replies of bot to DB
     '''
+    
     qry = "INSERT INTO mb_logs (user_id, message, reply, timestamp) VALUES (?, ?, ?)"
     time = datetime.datetime.now()
     time = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -73,13 +98,11 @@ def check_user(name, birth, nric=None):
     Get user line_id with `user_name` and `user_bday`
     Returns matched line_id
     '''
-    conn = connect_db(path)
-
     qry = """SELECT line_id FROM mb_user WHERE name=? and birth=?"""
-    result = var_query(conn, qry, (name, birth))
-
+    result = query(conn, qry, (name, birth))
     return result
 
 # Unit test for database
 if __name__ == "__main__":
+    conn = connect()
     pass
