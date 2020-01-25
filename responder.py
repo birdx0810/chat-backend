abf254508ce4abaef4f8335e6dc9deea7bde36d3
birdx0810 <birdx0810@gmail.com>
2020-01-25 16:51:13 +0800

    Debug registration message error

--- responder.py
+++ responder.py
@@ -42,7 +42,7 @@ def registration_resp(event, stat, session):
     '''
     msg_r = {
         'r0': "請輸入您的姓名",
-        'r1':,"請輸入您的生日（年年年年月月日日）"
+        'r1': "請輸入您的生日（年年年年月月日日）"
     }
 
     if stat == 'r0':
