663413fdc74b2d02981595e16249c37e9ae8ce6f
birdx0810 <birdx0810@gmail.com>
2020-06-05 23:06:21 +0800

    Resolved datetime errors

--- database.py
+++ database.py
@@ -82,8 +82,9 @@ def log(userid, message, direction):
     Log user messages and the replies of bot to DB
     '''
     qry = "INSERT INTO mb_logs (user_id, message, direction, timestamp) VALUES (%s, %s, %s, %s)"
-    time = datetime.datetime.now()
+    time = datetime.datetime.now().timestamp()
     # time = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(time)
     update(qry, (userid, message, direction, time))
     if direction == 0:
         direction = "FROM"
